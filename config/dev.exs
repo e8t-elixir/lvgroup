d4bd0929c7080e181924e3cfbcedc73a9943c9f7
Peter Lau <superpeterlau@outlook.com>
2021-10-24 06:46:04 +0800

    init auth; change password length

--- config/dev.exs
+++ config/dev.exs
@@ -15,7 +15,11 @@ config :app, App.Repo,
 config :app, AppWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  # http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [
+    ip: {0, 0, 0, 0},
+    port: (System.get_env("PORT") || "4000") |> String.to_integer()
+  ],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
